bd43315d3da92cf5776bed7e306d7625949872c0
Sricharan Reddy Varra <sricharanvarra@gmail.com>
2020-09-12 23:18:19 -0600

    Got the file names to be saved into an ArrayList

--- Project 1/src/com/company/AnimalNames.java
+++ Project 1/src/com/company/AnimalNames.java
@@ -14,40 +14,46 @@ public class AnimalNames {
     private List<String> FemaleNames = new ArrayList<String>();
 
     // Setters
-    private void SetMaleNamesPath(String file_path) {
+    protected void SetMaleNamesPath(String file_path) {
         this.MaleNamesPath = file_path;
     }
 
-    private void SetFemaleNamesPath(String file_path) {
+    protected void SetFemaleNamesPath(String file_path) {
         this.FemaleNamesPath = file_path;
     }
 
     // Getters
-
+//    private String GetRandomName(String sex) {
+//
+//    }
 
 
     // Other Functions
-    private static List<String> read_names(String file_path) throws FileNotFoundException {
+    private static List<String> ReadNames(String file_path) throws FileNotFoundException {
 
         List<String> names = new ArrayList<String>();
+        String line;
 
-
-        BufferedReader br = new BufferedReader(new FileReader(file_path)) {
-            String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(file_path))) {
             while((line = br.readLine()) != null) {
                 names.add(line);
             }
-        };
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done reading the file: " + file_path);
         return names;
     }
 
-    private void set_names(String sex) throws FileNotFoundException {
+    public void SetNames(String sex) throws FileNotFoundException {
 
         if ((sex == "M") || (sex == "Male")) {
-            this.MaleNames = read_names(this.MaleNamesPath);
+            this.MaleNames = ReadNames(this.MaleNamesPath);
         }
         else if ((sex == "F") || (sex == "Female")) {
-            this.FemaleNames = read_names(this.FemaleNamesPath);
+            this.FemaleNames = ReadNames(this.FemaleNamesPath);
         }
+        return;
     }
 }
--- Project 1/src/com/company/Main.java
+++ Project 1/src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import com.company.AnimalNames;
+import java.io.FileNotFoundException;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Read in the names of the animals
+        AnimalNames names = new AnimalNames();
+
+        names.SetMaleNamesPath("data/names/animal_names_M.csv");
+        names.SetFemaleNamesPath("data/names/animal_names_F.csv");
+
+        try {
+            names.SetNames("M");
+            names.SetNames("F");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
