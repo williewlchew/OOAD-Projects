90fb17243c53b3b7832c6191a615b3b2905ec5d2
Willie Chew <37847148+williewlchew@users.noreply.github.com>
2020-10-14 22:00:12 -0600

    tests

--- Project 3/src/roll/store/Main.java
+++ Project 3/src/roll/store/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // testing rolls
         Simulation sim = new Simulation(7);
         String buffer = sim.RunSimulation();
         System.out.println(buffer);
---
+++ Project 3/src/roll/store/StoreControllerTest.java
@@ -0,0 +1,29 @@
+package roll.store;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class StoreControllerTest {
+
+    MessageBean storeBean = new MessageBean();
+    Store store = new Store(storeBean);
+    StoreController storeController = new StoreController(store);
+
+    @Test
+    public void CheckMadeRolls1() {
+        Roll testRoll1 = storeController.SpringRollSlot();
+        assertEquals("Spring Roll", testRoll1.getDescription());
+        assertEquals(1.00, testRoll1.getCost());
+    }
+
+    @Test
+    public void CheckMadeRolls2() {
+        Roll testRoll2 = storeController.PastryRollSlot();
+        testRoll2 = storeController.ExtraSauceSlot(testRoll2, "Sour sauce");
+        assertEquals("Pastry Roll with Sour sauce, ",  testRoll2.getDescription());
+        testRoll2 = storeController.ExtraToppingSlot(testRoll2, "Colorful toppings");
+        assertEquals("Pastry Roll with Sour sauce, Colorful toppings, ", testRoll2.getDescription());
+        assertEquals(4.00, testRoll2.getCost());
+    }
+}
